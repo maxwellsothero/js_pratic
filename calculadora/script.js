5611a64bfa427c666d504b4bdc41c697fb09d551
Maxwell Sothero <localizameucarro@gmail.com>
2022-06-27 14:07:29 -0300

    adicionando a função de subtrair numeros negativos

--- calculadora/script.js
+++ calculadora/script.js
@@ -5,13 +5,13 @@ let opeVar = '';
 let gravaMemoria = '';
 
 function selecionarDigito(numero){
-    if(parseFloat(VISOR.innerHTML)=== 0 ){
-        VISOR.innerHTML = '';
-    }    
+    if(parseFloat(VISOR.innerHTML)=== 0){
+        VISOR.innerHTML = '';       
+    } 
     if(operacao === true){
         operacao = false;
-        VISOR.innerHTML = '';
-    }           
+        VISOR.innerHTML = '';        
+    }
     VISOR.innerHTML += numero;
 }
 function resultado(){
@@ -36,8 +36,13 @@ function somar(){
     operacao = true;
     memoriaAuxiliar = parseFloat(VISOR.innerHTML); 
 }
-function subtrair(){
+function subtrair(){   
     opeVar = '-';
+            if(parseFloat(VISOR.innerHTML)=== 0 ){
+                VISOR.innerHTML = opeVar;
+                
+                selecionarDigito(numero);//função ta dando erro:
+    }  
             if (memoriaAuxiliar !== 0) {  
             VISOR.innerHTML = memoriaAuxiliar - parseFloat(VISOR.innerHTML);                  
     }
@@ -45,14 +50,13 @@ function subtrair(){
     memoriaAuxiliar = parseFloat(VISOR.innerHTML);
 }
 function multiplicar(){
-    opeVar = '*'
-    if (memoriaAuxiliar !== 0) {   
-             
-        VISOR.innerHTML = memoriaAuxiliar * parseFloat(VISOR.innerHTML);                  
-    }
-    operacao = true;
-    memoriaAuxiliar = parseFloat(VISOR.innerHTML);
-}
+            opeVar = '*';
+            if (memoriaAuxiliar !== 0) {                     
+                VISOR.innerHTML = memoriaAuxiliar * parseFloat(VISOR.innerHTML);                  
+            }
+            operacao = true;
+            memoriaAuxiliar = parseFloat(VISOR.innerHTML);
+        }
 function dividir(){
     opeVar = '/'
     if (memoriaAuxiliar !== 0) {                
